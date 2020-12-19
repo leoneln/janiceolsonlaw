90923c11954d71fb5886ea149cdad0808267c903
Leonel Nieto <leonelnieto@gmail.com>
2020-12-19 15:56:58 -0700

    Added all information needed

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,12 +1,14 @@
-(function($) {
+//const { error } = require("jquery");
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+  $("body").on("input propertychange", ".floating-label-form-group", function (e) {
     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
+  }).on("focus", ".floating-label-form-group", function () {
     $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
+  }).on("blur", ".floating-label-form-group", function () {
     $(this).removeClass("floating-label-form-group-with-focus");
   });
 
@@ -17,9 +19,9 @@
   if ($(window).width() > MQL) {
     var headerHeight = $('#mainNav').height();
     $(window).on('scroll', {
-        previousTop: 0
-      },
-      function() {
+      previousTop: 0
+    },
+      function () {
         var currentTop = $(window).scrollTop();
         //check if user is scrolling up
         if (currentTop < this.previousTop) {
@@ -37,5 +39,25 @@
         this.previousTop = currentTop;
       });
   }
+
+  //Print year on copyright
+  let date = new Date();
+  let year = date.getFullYear();
+  document.querySelector('#copyright-year').innerHTML = year;
+
+  //copy email to clipboard
+  document.querySelector('#copy-to-clipboard').addEventListener('click', async event => {
+    if (!navigator.clipboard) {
+      //clipboard api not available
+      return
+    }
+    const text = document.querySelector('#copy-to-clipboard-text').innerHTML;
+    try {
+      await navigator.clipboard.writeText(text)
+      document.querySelector("#copy-to-clipboard-raplace").innerHTML = 'Email copied to clipboard';
+    } catch (err) {
+      console.error('Failed to copy!', err);
+    }
+  });
 
 })(jQuery); // End of use strict
